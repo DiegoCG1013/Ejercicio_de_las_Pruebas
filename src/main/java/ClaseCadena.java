f882839a2f2a6fdb4fdfdd71cb9e857a5824149e
Diego <diegocarrasco1013@gmail.com>
2023-03-23 16:08:09 +0100

    Actualizacion

--- src/main/java/ClaseCadena.java
+++ src/main/java/ClaseCadena.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class ClaseCadena {
+public class ClaseCadena {
+
+    static final char[] letras = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+
+
+    public static String DevuelveIniciales(String nombre){
+        try {
+            String[] palabras = nombre.split(" ");
+            if (palabras.length != 2) throw new Exception("Error en los espacios");
+            char[] iniciales = {palabras[0].charAt(0), palabras[1].charAt(0)};
+            if (!esLetra(iniciales[0]) || !esLetra(iniciales[1])) throw new Exception("Error en las iniciales");
+            return iniciales[0] + "." + iniciales[1] + ".";
+        } catch (Exception e) {
+        }
+        return "";
+    }
+
+    public static boolean esLetra(char letra){
+        for (char c : letras) {
+            if (c == letra) return true;
+        }
+        return false;
+    }
 }
